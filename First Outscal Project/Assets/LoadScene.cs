258d460c1e8696306148cc1d191fc55079b70681
Abhishek-D-Developer <61371395+Abhishek-D-Developer@users.noreply.github.com>
2020-02-26 17:04:59 +0530

    Feature1-buttons

---
+++ First Outscal Project/Assets/LoadScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	public void loadscene1()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void loadscene2()
+	{
+		SceneManager.LoadScene (2);
+	}
+
+	public void loadscene3()
+	{
+		SceneManager.LoadScene (3);
+	}
+
+	public void MasterScene()
+	{
+		SceneManager.LoadScene (0);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+		Debug.Log ("quit");
+	}
+}
